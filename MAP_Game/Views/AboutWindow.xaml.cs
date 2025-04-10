1b7cd8dd53d52f529bbd6fa7dfe21d790eb6daf9
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2025-04-11 00:00:33 +1400

    Minor changes

--- MAP_Game/Views/AboutWindow.xaml.cs
+++ MAP_Game/Views/AboutWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
 
 namespace MAP_Game.View
 {
@@ -7,6 +11,31 @@ namespace MAP_Game.View
         public AboutWindow()
         {
             InitializeComponent();
+        }
+
+        private void EmailHyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string email = "email@gmail.ro";
+                string subject = "Regarding Tavern Chronicles Game";
+                string body = "Dear Tavern Keeper,\n\n";
+
+                string mailtoUri = $"mailto:{email}?subject={Uri.EscapeDataString(subject)}&body={Uri.EscapeDataString(body)}";
+
+                Process.Start(new ProcessStartInfo(mailtoUri)
+                {
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not open email client: {ex.Message}",
+                              "Error",
+                              MessageBoxButton.OK,
+                              MessageBoxImage.Error);
+            }
         }
     }
 }
